4ecd9b7a8854285bf30108f14dad180fe2c8ee0d
lyssadev <toolsplus1000@gmail.com>
2026-08-31 21:21:56 +0000

    limit abis to armv7 & armv8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
         }
     }
 
