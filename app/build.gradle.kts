d140645b34531dfac4d5d4800a6bdfec9a2e12d4
lyssadev <toolsplus1000@gmail.com>
2026-09-01 23:42:53 +0000

    bump to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "mint.app"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
