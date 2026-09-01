79a29a76670c5b2d733ccc9779acd8fb954dd112
lyssadev <toolsplus1000@gmail.com>
2026-09-01 17:42:03 +0000

    let okhttp handle gzip decompression in tiktokresolver

--- app/src/main/java/mint/app/resolution/impl/TikTokResolver.kt
+++ app/src/main/java/mint/app/resolution/impl/TikTokResolver.kt
@@ -101,7 +101,6 @@ object TikTokResolver : Resolver {
             .header("User-Agent", UA)
             .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .header("Accept-Language", "en-US,en;q=0.5")
-            .header("Accept-Encoding", "gzip, deflate, br")
             .header("Sec-Fetch-Dest", "document")
             .header("Sec-Fetch-Mode", "navigate")
             .header("Sec-Fetch-Site", "none")
